69164ef4ed147dbd084003e291942215e9be97d3
tsubasasugawara <sugawara1740@gmail.com>
2023-04-05 12:41:37 +0000

    配列の初期化関数を追加

--- src/evaluator/builtins.go
+++ src/evaluator/builtins.go
@@ -16,4 +16,28 @@ var builtins = map[string]*object.Builtin {
 			return NULL
 		},
 	},
+	"配列": &object.Builtin {
+		Fn: func(args ...object.Object) object.Object {
+			var defVal object.Object
+
+			if len(args) == 1 {
+				defVal = NULL
+			} else if len(args) == 2 {
+				defVal = args[1]
+			} else {
+				return newError("引数の個数が合っていません。")
+			}
+
+			length, ok := args[0].(*object.Integer)
+			if !ok {
+				return newError("数値が必要です")
+			}
+
+			elements := make([]object.Object, length.Value)
+			for i, _ := range elements {
+				elements[i] = defVal
+			}
+			return &object.Array{Elements: elements}
+		},
+	},
 }
